838419fb9e4432efdce2a7104f348780e865e7a2
shakinartem <sakinartem28@gmail.com>
2026-08-17 16:56:21 +0400

    add platform adapter package

---
+++ autoposter_bot/platforms/__init__.py
@@ -0,0 +1,15 @@
+from autoposter_bot.platforms.base import (
+    CapabilitySpec,
+    PlatformAdapter,
+    PublicationResult,
+    ValidationIssue,
+)
+from autoposter_bot.platforms.registry import PlatformRegistry
+
+__all__ = [
+    "CapabilitySpec",
+    "PlatformAdapter",
+    "PlatformRegistry",
+    "PublicationResult",
+    "ValidationIssue",
+]
